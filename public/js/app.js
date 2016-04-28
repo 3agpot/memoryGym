b22e4278047603753d6ed19c466d5fbb723f1bfd
Rashid <rashid@leadpops.com>
2016-04-28 01:42:34 -0700

    switched to angular-resource for an API adapter, cleaned up the code

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,17 @@
 var gymApp = angular.module("gymApp", ['ui.router', 'ngResource']);
 
-gymApp.service('resourceService', ['$resource', function($resource){
-    return $resource('http://jsonplaceholder.typicode.com/users/:user',{user: "@user"});
-}]);
-
-gymApp.service('faceService', function($http, $q){
-    var deferred = $q.defer();
-    $http.get('http://localhost:8080/faces').then(function(data){
-        deferred.resolve(data);
-    });
-    
-    this.getFaces = function(){
-        return deferred.promise;
-    }
-});
-
-gymApp.factory('Phone', ['$resource',
+gymApp.factory('faces', ['$resource',
     function($resource){
-        return $resource('http://jsonplaceholder.typicode.com/users/:user', {}, {
-            query: {method:'GET', params:{user:'1'}, isArray:false}
+        return $resource('http://localhost:8080/faces', {}, {
+            query: {method:'GET', isArray:true}
         });
 }]);
 
-gymApp.controller('NamesAndFacesController',[ 'faceService', 'resourceService', 'Phone', '$http', '$q', function(faceService, resourceService, Phone, $http, $q){
-    this.faces= [{"name":{"first":"John", "last":"Smith"}}, {"name":{"first":"Jane", "last":"Smith"}}];
+gymApp.controller('NamesAndFacesController',['faces', function(faces){
+    //this.faces= [{"name":{"first":"John", "last":"Smith"}}, {"name":{"first":"Jane", "last":"Smith"}}];
+
     this.loadFaces = function(){
-        var promise = faceService.getFaces();
-        promise.then(function(data){
-            this.faces = data.data;
-            console.log(this.faces);
-        });
-    }
-
-    this.show = function() {
-        alert(this.faces[0].name.first);
-    }
-
-    this.tmp = function(){
-        $http.get('http://localhost:8080/faces').then(function(data){
-            this.faces = angular.fromJson(data.data);
-            console.log(this.faces);
-        });
-    }
-
-    this.showDummy = function(){
-        this.users = Phone.query();
-        alert(this.users);
+        this.faces = faces.query();
     }
 
     this.people = [
@@ -59,20 +25,6 @@ gymApp.controller('NamesAndFacesController',[ 'faceService', 'resourceService', 
         this.newPerson = '';
     };
 }]);
-
-
-/*gymApp.service('facesService', function($http){
-    //delete $http.defaults.headers.common['X-Requested-With'];
-    this.getData = function() {
-        // $http() returns a $promise that we can add handlers with .then()
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:8080/faces',
-            params: 'limit=10'
-            //headers: {'Authorization': 'Token token=xxxxYYYYZzzz'}
-         });
-     }
-});*/
 
 gymApp.config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise("/");
